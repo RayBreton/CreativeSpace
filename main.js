341807de292ef947621577e1f607bb358a016b2f
Raynier Breton <8505373+RayBreton@users.noreply.github.com>
2018-02-25 01:29:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var red = [0, 125, 35];
+var orange = [40, 100, 60];
+var green = [500, 400, 24];
+var blue = [200, 150, 30];
+var purple = [280, 50, 60];
+
+var myName = "RBreton";
+
+if (10 < 3) {
+    bubbleShape = "circle";
+}
+
+else {
+    bubbleShape = "square";
+}
+
+letterColors = [blue, red, orange, green, red]
+
+drawName(myName, letterColors);
+bounceBubbles()
